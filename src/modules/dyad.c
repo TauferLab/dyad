18b0497e7ddfe6d0faea3932a65284c8f9d19a26
ilumsden <lumsden.ian@gmail.com>
2023-04-20 11:06:00 -0400

    Fixes typo

--- src/modules/dyad.c
+++ src/modules/dyad.c
@@ -264,7 +264,7 @@ int mod_main (flux_t *h, int argc, char **argv)
     mkdir_as_needed (ctx->dyad_path, m);
 
     if (argc == 2) {
-        FLUX_LOG_INO (h, "DTL Mode (from cmd line) is %s\n", argv[1]);
+        FLUX_LOG_INFO (h, "DTL Mode (from cmd line) is %s\n", argv[1]);
         flag_len = strlen(argv[1]);
         if (strncmp (argv[1], "FLUX_RPC", flag_len) == 0) {
             dtl_mode = DYAD_DTL_FLUX_RPC;
